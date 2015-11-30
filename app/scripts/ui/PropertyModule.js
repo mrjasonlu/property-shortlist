14e7829b2834ab070731798877c66202e58de4e7
Jason Lu <jlu@squiz.net>
2015-11-30 20:33:36 +1100

    updated add / remove logic so properties don't get removed from results

--- app/scripts/ui/PropertyModule.js
+++ app/scripts/ui/PropertyModule.js
@@ -5,31 +5,28 @@ var React = require('react'),
     SavedList = require('./SavedList');
 
 var PropertyModule = React.createClass({
+    //function to remove property from saved list and add back into results
     removeProperty: function(key) {
-      var newPropertyList = this.state.propertyList;
-      var removedProperty = this.state.savedList[key];
-      newPropertyList.splice(0,0,removedProperty);
-      this.setState({propertyList: newPropertyList});
       var newSavedList = this.state.savedList;
       newSavedList.splice(key, 1);
       this.setState({savedList: newSavedList});
     },
+    //function to add property to saved list and remove from results
     addProperty: function(key) {
       var newSavedList = this.state.savedList;
       var newProperty = this.state.propertyList[key];
-      newSavedList.splice(0,0,newProperty);
+      newSavedList.push(newProperty);
       this.setState({savedList: newSavedList});
-      var newPropertyList = this.state.propertyList;
-      newPropertyList.splice(key, 1);
-      this.setState({propertyList: newPropertyList});
     },
     getInitialState: function(){
+        //set initial property and saved list from JSON data
         return {
           propertyList: this.props.propertyData.results,
           savedList: this.props.propertyData.saved
         }
     },
     render: function() {
+        //render results and saved list
         var propModule = this;
         return (
           <div>
