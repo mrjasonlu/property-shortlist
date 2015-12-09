5c65e294d2c3609912c76f6e1c4573190988dc0d
Jason Lu <jlu@squiz.net>
2015-12-09 21:21:17 +1100

    fixed issue with click propagation deleting multiple properties

--- app/scripts/ui/PropertyItem.js
+++ app/scripts/ui/PropertyItem.js
@@ -4,13 +4,16 @@ var React = require('react')
 
 var PropertyItem = React.createClass({
     //function to handle user click, and decide whether it should be added or removed from saved list
-    handleClick: function() {
+    handleClick: function(e) {
         var props = this.props;
         if(props.saved){ //if property is in saved list, remove from list
+            console.log("delete "+props.arrayKey);
             props.removeProperty(props.arrayKey);
         } else { //if property is not saved, save to list
             props.addProperty(props.arrayKey);
         }
+        e.stopPropagation();
+        e.nativeEvent.stopImmediatePropagation();
     },
     render: function() {
       var agentStyle = { backgroundColor: this.props.propData.agency.brandingColors.primary};
